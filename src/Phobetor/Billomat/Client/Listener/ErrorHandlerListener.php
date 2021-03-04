a33fa829b311fe7821167268e030c63c751bb0e3
Jakob Schumann <jakob.schumann@vrok.de>
2021-03-04 17:17:21 +0100

    upd: ErrorHandler upd (c) @scratchy

--- src/Phobetor/Billomat/Client/Listener/ErrorHandlerListener.php
+++ src/Phobetor/Billomat/Client/Listener/ErrorHandlerListener.php
@@ -25,6 +25,8 @@ class ErrorHandlerListener implements EventSubscriberInterface
     const STATUS_UNAUTHORIZED       = 401;
     const STATUS_NOT_FOUND          = 404;
     const STATUS_TOO_MANY_REQUESTS  = 429;
+    const SERVER_DOWN               = 503;
+    const SERVER_DOWN_2             = 504;
 
     /**
      * {@inheritDoc}
@@ -92,6 +94,8 @@ class ErrorHandlerListener implements EventSubscriberInterface
             case self::STATUS_UNAUTHORIZED:
                 return new UnauthorizedException($errorName, $statusCode);
             case self::STATUS_TOO_MANY_REQUESTS:
+            case self::SERVER_DOWN:
+            case self::SERVER_DOWN_2:
                 $exception = new TooManyRequestsException($errorName, $statusCode);
                 if ($response->hasHeader('X-Rate-Limit-Remaining')) {
                     $exception->setRateLimitRemaining((int)(string)$response->getHeader('X-Rate-Limit-Remaining'));
